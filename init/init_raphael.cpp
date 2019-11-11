31b9921c9f7c56a53b049ce47b29cb7d23e42ef3
UtsavisGreat <utsavbalar1231@gmail.com>
2019-11-11 03:33:34 +0100

    raphael: override privapp permissions to log for all variants
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -40,6 +40,7 @@ void load_raphaelglobal() {
     property_override("ro.product.device", "raphael");
     property_override("ro.build.description", "raphael-user 9 PKQ1.181121.001 V10.3.1.0.PFKEUXM release-keys");
     property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override("ro.control_privapp_permissions", "log");
 }
 
 void load_raphaelin() {
@@ -48,6 +49,7 @@ void load_raphaelin() {
     property_override("ro.product.device", "raphaelin");
     property_override("ro.build.description", "raphaelin-user 9 PKQ1.181121.001 V10.3.3.0.PFKINXM release-keys");
     property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override("ro.control_privapp_permissions", "log");
 }
 
 void load_raphael() {
@@ -56,6 +58,7 @@ void load_raphael() {
     property_override("ro.product.device", "raphael");
     property_override("ro.build.description", "raphael-user 9 PKQ1.181121.001 V10.3.12.0.PFKCNXM release-keys");
     property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override("ro.control_privapp_permissions", "log");
 }
 
 
